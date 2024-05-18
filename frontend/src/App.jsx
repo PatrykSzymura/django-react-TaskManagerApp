47246b04e45a636c3889c09620b8834908bc11e3
PatrykSzymura <121195581+PatrykSzymura@users.noreply.github.com>
2024-05-18 14:11:05 +0200

    Addinnng Protected routes and base form for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
 //import React , {useState, useEffect} from "react";
 import './App.css';
-//import Projects from'./pages/Projects'
+import Projects from'./pages/Projects'
+import PrivateRoutes from './utils/PrivateRoutes'
 import { LoginPage , RegisPage} from "./pages/LogReg";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
   
   return (
     <div className="App">
-      Task Manager
-      <LoginPage/>
+      <Router>
+        <Routes>
+          <Route element={<LoginPage/>} path='/login'/>
+          <Route element={<PrivateRoutes/>}>
+          <Route path='/projects' element={<Projects/>} />
+          </Route>
+          <Route path='*' element={<Navigate to='/login' />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
