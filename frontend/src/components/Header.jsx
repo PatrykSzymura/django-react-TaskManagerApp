5d1ac87e5dba5b11ed1fb21b28376098454e2fa9
PiterBiter <121134220+PiterBiter@users.noreply.github.com>
2024-05-23 11:01:20 +0200

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         </div>
         <div className="flex gap-4">
           <Link to="/Projects" className="hover:underline">Projects</Link>
-          <Link to="/teams" className="hover:underline">Teams</Link>
+          <Link to="/CreateTeam" className="hover:underline">Manage Teams</Link>
           <Link to="/users" className="hover:underline">Users</Link>
           <Link to="/contact" className="hover:underline">Contact</Link>
           <Link to="/contact" className="hover:underline">Log Out</Link>
