5b657db29cbce575422f89ec510a86881fcef65e
PiterBiter <121134220+PiterBiter@users.noreply.github.com>
2024-05-24 03:45:35 +0200

    Update Tasks.jsx

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,28 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const TaskList = () => {
-    const [projects, setProjects] = useState([]);
+const TaskList = ({ project }) => { 
     const [tasks, setTasks] = useState([]);
-    const [loadingProjects, setLoadingProjects] = useState(true);
     const [loadingTasks, setLoadingTasks] = useState(false);
     const [error, setError] = useState(null);
-    const [selectedProject, setSelectedProject] = useState('');
-
-    useEffect(() => {
-        const fetchProjects = async () => {
-            try {
-                const response = await axios.get('http://localhost:8000/api/get/projects');
-                setProjects(response.data);
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoadingProjects(false);
-            }
-        };
-
-        fetchProjects();
-    }, []);
 
     const fetchTasks = async (projectId) => {
         setLoadingTasks(true);
@@ -36,38 +18,15 @@ const TaskList = () => {
         }
     };
 
-    const handleProjectChange = (e) => {
-        const projectId = e.target.value;
-        setSelectedProject(projectId);
-        if (projectId) {
-            fetchTasks(projectId);
+    useEffect(() => {
+        if (project) {
+            fetchTasks(project.id); 
         }
-    };
+    }, [project]);
 
-    if (loadingProjects) return <p>Loading projects...</p>;
-    if (error) return <p>Error: {error}</p>;
-
-    return (
+    if (!project) return <p>No project selected</p>; 
+   return (
         <div>
-            <div className="mb-4">
-                <label htmlFor="projects" className="block text-sm font-medium text-gray-700">
-                    Select Project:
-                </label>
-                <select
-                    id="projects"
-                    name="projects"
-                    className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    value={selectedProject}
-                    onChange={handleProjectChange}
-                >
-                    <option value="">Select a project</option>
-                    {projects.map((project) => (
-                        <option key={project.id} value={project.id}>
-                            {project.project_name}
-                        </option>
-                    ))}
-                </select>
-            </div>
             <div className="overflow-x-auto">
                 {loadingTasks ? (
                     <p>Loading tasks...</p>
